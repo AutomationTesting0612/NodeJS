b8b081287f32df736e13f8b2a1fe1a82877f5b96
AutomationTesting0612 <automationtesting0612@gmail.com>
2024-01-12 15:08:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res)=>{
+    res.send("techlearn023");
+
+})
+
+app.get('/login',(req, res)=>{
+    res.send('<h1>This is the login page for first project</h1>');
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
